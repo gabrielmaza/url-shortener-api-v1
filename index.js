8181ca3f13b6cac3d1b647e657a6d857e0cbc279
gmazasilentium <80764590+gmazasilentium@users.noreply.github.com>
2023-11-20 00:01:13 -0300

    Modificación cors Fixes #1

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (whiteList.includes(origin)) {
+      console.log("😲😲😲 => ", origin);
+      if (!origin || whiteList.includes(origin)) {
         return callback(null, origin);
       }
-      return callback("Error de CORS: " + origin + " origin no autorizado");
+      return callback("Error de CORS: " + origin + " no autorizado");
     },
   })
 );
